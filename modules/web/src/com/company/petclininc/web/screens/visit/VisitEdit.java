45f58ae3f573b9b3d6530ff4054c388429f00943
Andrey Belyaev <belyaev.andrey@gmail.com>
2021-02-17 11:08:52 +0300

    Added JS editor

--- modules/web/src/com/company/petclininc/web/screens/visit/VisitEdit.java
+++ modules/web/src/com/company/petclininc/web/screens/visit/VisitEdit.java
@@ -2,13 +2,16 @@ package com.company.petclininc.web.screens.visit;
 
 import com.company.petclininc.entity.Consumable;
 import com.company.petclininc.service.VisitService;
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.gui.model.CollectionChangeType;
 import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.petclininc.entity.Visit;
+import com.haulmont.cuba.web.gui.components.JavaScriptComponent;
 
 import javax.inject.Inject;
+import java.util.Map;
 
 @UiController("petclininc_Visit.edit")
 @UiDescriptor("visit-edit.xml")
@@ -16,14 +19,30 @@ import javax.inject.Inject;
 @LoadDataBeforeShow
 public class VisitEdit extends StandardEditor<Visit> {
 
-    private Visit visit;
-
-    public void setVisit(Visit visit) {
-        this.visit = visit;
-    }
-
     @Inject
     private VisitService visitService;
+    @Inject
+    private JavaScriptComponent descriptionEdit;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        QuillData data = new QuillData();
+        data.options = ParamsMap.of("theme", "snow", "placeholder", "");
+        descriptionEdit.setState(data);
+
+        descriptionEdit.addFunction("valueChanged", javaScriptCallbackEvent -> {
+            String value = javaScriptCallbackEvent.getArguments().getString(0);
+            getEditedEntity().setDescription(value);
+        });
+
+    }
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        descriptionEdit.callFunction("setText", getEditedEntity().getDescription());
+    }
+
+
 
     @Subscribe(id = "visitDc", target = Target.DATA_CONTAINER)
     public void onVisitDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Visit> event) {
@@ -37,6 +56,10 @@ public class VisitEdit extends StandardEditor<Visit> {
         if (event.getChangeType() != CollectionChangeType.REFRESH) {
             getEditedEntity().setAmount(visitService.calculateAmount(getEditedEntity()));
         }
+    }
+
+    class QuillData {
+        public Map<String, Object> options;
     }
 
 }
