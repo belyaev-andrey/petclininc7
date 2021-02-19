6999b2a6c2bc55d656d4a70bfaa82447db0124a1
Andrey Belyaev <belyaev.andrey@gmail.com>
2021-02-19 09:58:39 +0300

    Removed unused fields

--- modules/core/src/com/company/petclininc/core/listeners/TreatSuccessListener.java
+++ modules/core/src/com/company/petclininc/core/listeners/TreatSuccessListener.java
@@ -5,11 +5,8 @@ import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.DataManager;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.ExecutionListener;
-import org.springframework.stereotype.Component;
 
 public class TreatSuccessListener implements ExecutionListener {
-
-    public static final String NAME = "petclininc_TreatSuccessListener";
 
     public void notify(DelegateExecution execution) {
         Visit visit = (Visit)execution.getVariable("visit");
